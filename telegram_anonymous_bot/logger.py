23c0d561bf30b3ee002a4f3e8158b3df27c2cddd
hosein <hosienlooki0@gmail.com>
2022-02-06 22:46:26 +0330

    fix logs

--- telegram_anonymous_bot/logger.py
+++ telegram_anonymous_bot/logger.py
@@ -10,7 +10,7 @@ log_file = PATH_STORAGE.joinpath('error_logs.txt')
 handler = logging.handlers.RotatingFileHandler(
     log_file, maxBytes=20 * 1000 * 1000, backupCount=1, )
 handler.setFormatter(logging.Formatter(FORMAT))
-error_logger = logging.getLogger(log_file)
+error_logger = logging.getLogger(str(log_file))
 error_logger.propagate = True
 error_logger.setLevel(logging.ERROR)
 error_logger.addHandler(handler)
@@ -21,7 +21,7 @@ log_file = PATH_STORAGE.joinpath('info_logs.txt')
 handler = logging.handlers.RotatingFileHandler(
     log_file, maxBytes=20 * 1000 * 1000, backupCount=1, )
 handler.setFormatter(logging.Formatter(FORMAT))
-info_log = logging.getLogger(log_file)
+info_log = logging.getLogger(str(log_file))
 info_log.propagate = True
 info_log.setLevel(TELEGRAM_LOG_LEVEL)
 info_log.addHandler(handler)
